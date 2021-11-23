b804772f54c0b6817da5f77536f050d9d83f704f
thilyvu <thilyvu.2000@gmail.com>
2021-11-24 00:38:26 +0700

    Add comment

--- models/score.js
+++ models/score.js
@@ -1,44 +1,50 @@
-const { Schema, model } = require ('mongoose')
-const ScoreSchema = new Schema({
-  answerFile:{
-    type : String,
-    default : null
+const { Schema, model } = require("mongoose");
+const ScoreSchema = new Schema(
+  {
+    answerFile: {
+      type: String,
+      default: null,
+    },
+    exerciseId: {
+      type: String,
+      required: true,
+    },
+    studentId: {
+      type: String,
+      required: true,
+    },
+    student: {
+      type: Object,
+    },
+    scores: {
+      type: String,
+    },
+    fileExtension: {
+      type: String,
+    },
+    createName: {
+      type: String,
+      default: null,
+    },
+    createAvatar: {
+      type: String,
+      default: null,
+    },
+    comment: {
+      type: String,
+    },
+    createBy: {
+      type: String,
+      default: null,
+    },
+    updateBy: {
+      type: String,
+      default: null,
+    },
+    fileName: {
+      type: String,
+    },
   },
-  exerciseId : { 
-    type: String, 
-    required : true
-  },
-  studentId : {
-    type: String, 
-    required : true
-  },
-  student : {
-    type : Object
-  },
-  scores : {
-    type : String,
-  },
-  fileExtension : {
-    type : String,
-  },
-  createName :{
-    type : String,
-    default : null
-  },
-  createAvatar :{
-    type : String,
-    default : null
-  },
-  createBy : {
-    type : String,
-    default : null
-  },
-  updateBy : {
-    type: String,
-    default : null
-  },
-  fileName : {
-    type : String,
-  }
-},{timestamps : true})
-module.exports = model('score', ScoreSchema)
+  { timestamps: true }
+);
+module.exports = model("score", ScoreSchema);
