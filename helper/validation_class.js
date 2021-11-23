5e3f99e6839b6da509ed8a18de3b5c33ae1d8ed6
thilyvu <thilyvu.2000@gmail.com>
2021-11-24 00:41:27 +0700

    Add comment

--- helper/validation_class.js
+++ helper/validation_class.js
@@ -1,38 +1,39 @@
-const Joi = require('joi')
+const Joi = require("joi");
 
 const classCreateSchema = Joi.object().keys({
-  name :Joi.string().trim().required(),
+  name: Joi.string().trim().required(),
   classCode: Joi.required(),
   description: Joi.required(),
   bannerImg: Joi.required(),
   approveMode: Joi.boolean().required(),
   subject: Joi.string().required(),
-  grade : Joi.string().required(),
-})
+  grade: Joi.string().required(),
+  province: Joi.string(),
+});
 
 const classUpdateSchema = Joi.object().keys({
   name: Joi.string().trim().required(),
   isHide: Joi.boolean(),
-  description:Joi.string().trim(),
+  description: Joi.string().trim(),
   bannerImg: Joi.string().trim(),
-  approveMode:Joi.boolean(),
+  approveMode: Joi.boolean(),
   subject: Joi.string(),
-  grade : Joi.string()
-
-})
+  grade: Joi.string(),
+  province: Joi.string(),
+});
 const addToClassSchema = Joi.object().keys({
-  classId : Joi.string().required(),
-  studentId: Joi.string().required()
-})
+  classId: Joi.string().required(),
+  studentId: Joi.string().required(),
+});
 
 const addToClassSchemaByEmailAndPhone = Joi.object().keys({
-  classId : Joi.string().required(),
+  classId: Joi.string().required(),
   phone: Joi.string(),
-  email : Joi.string()
-})
-module.exports= { 
-    classCreateSchema,
-    classUpdateSchema,
-    addToClassSchema,
-    addToClassSchemaByEmailAndPhone
-}
+  email: Joi.string(),
+});
+module.exports = {
+  classCreateSchema,
+  classUpdateSchema,
+  addToClassSchema,
+  addToClassSchemaByEmailAndPhone,
+};
--- helper/validation_score.js
+++ helper/validation_score.js
@@ -1,22 +1,23 @@
-const Joi = require('joi')
+const Joi = require("joi");
 
 const scoreCreateSchema = Joi.object().keys({
-    answerFile :Joi.string().required(),
-    exerciseId: Joi.string().required(),
-    studentId : Joi.string().required(),
-    fileExtension : Joi.string(),
-    fileName :Joi.string()
-})
+  answerFile: Joi.string().required(),
+  exerciseId: Joi.string().required(),
+  studentId: Joi.string().required(),
+  fileExtension: Joi.string(),
+  fileName: Joi.string(),
+});
 
 const scoreUpdateSchema = Joi.object().keys({
-    answerFile :Joi.string(),
-    exerciseId: Joi.string(),
-    studentId : Joi.string().required(),
-    scores : Joi.string(),
-    fileExtension : Joi.string(),
-    fileName :Joi.string()
-})
-module.exports= { 
-    scoreCreateSchema,
-    scoreUpdateSchema
-}
+  answerFile: Joi.string(),
+  exerciseId: Joi.string(),
+  studentId: Joi.string().required(),
+  scores: Joi.string(),
+  fileExtension: Joi.string(),
+  fileName: Joi.string(),
+  comment: Joi.string(),
+});
+module.exports = {
+  scoreCreateSchema,
+  scoreUpdateSchema,
+};
